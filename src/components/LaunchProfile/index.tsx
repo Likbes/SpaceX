96c7fdc3c344a7e70eac1cc38da2e5a73f743e77
Likbes <likbes.js@gmail.com>
2019-06-28 21:25:07 +0300

    create basic logic without interactive

---
+++ src/components/LaunchProfile/index.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { useLaunchProfileQuery } from '../../generated/graphql';
+import LaunchProfile from './LaunchProfile';
+
+const LaunchProfileContainer = () => {
+
+  const { data, loading, error } = useLaunchProfileQuery({ variables: { id: '42' } });
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>ERROR</div>;
+  }
+
+  if (!data) {
+    return <div>Select a flight from the panel</div>;
+  }
+
+  return <LaunchProfile data={data} />;
+};
+
+export default LaunchProfileContainer;
