51fe0952058cf068ed5eb0a822b10f7ab50d2493
Likbes <likbes.js@gmail.com>
2019-06-29 08:47:19 +0300

    Updates

---
+++ precache-manifest.f6f57c528b5f7164678f21ac898e8816.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d21e83f2f2c21bf85fe110540727f187",
+    "url": "/spacex/index.html"
+  },
+  {
+    "revision": "a040aa028e8c9fc22607",
+    "url": "/spacex/static/css/main.ed542491.chunk.css"
+  },
+  {
+    "revision": "d996426d8d4270c1029f",
+    "url": "/spacex/static/js/2.2fd10c8f.chunk.js"
+  },
+  {
+    "revision": "a040aa028e8c9fc22607",
+    "url": "/spacex/static/js/main.df49583f.chunk.js"
+  },
+  {
+    "revision": "d97e5a4bb81ec134a109",
+    "url": "/spacex/static/js/runtime~main.a706bf65.js"
+  }
+]);
